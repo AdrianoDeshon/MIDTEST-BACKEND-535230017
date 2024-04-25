cc9a6c07aa7a02fee5b21edd35935d0d36ae709a
AdrianoDeshon <adriano.535230017@stu.untar.ac.id>
2024-04-25 18:38:26 +0700

    Nomor 1 Update

--- src/api/components/users/users-repository.js
+++ src/api/components/users/users-repository.js
@@ -1,11 +1,57 @@
 const { User } = require('../../../models');
 
 /**
- * Get a list of users
+ * Get a list of users with pagination and filter
+ * @param {number} page_number - Page number
+ * @param {number} page_size - Page size
+ * @param {string} query - Query
+ * @param {string} sortOptions - Sort Options
  * @returns {Promise}
  */
-async function getUsers() {
-  return User.find({});
+async function getUsers({ page_number, page_size, query, sortOptions }) {
+  let usersQuery = User.find(query);
+
+  // Apply sorting
+  if (sortOptions && Object.keys(sortOptions).length !== 0) {
+    usersQuery = usersQuery.sort(sortOptions);
+  }
+
+  // Get total count of users
+  const count = await User.countDocuments(query);
+
+  // Calculate total pages
+  const total_pages = Math.ceil(count / page_size);
+
+  // Check if there is a previous page
+  const has_previous_page = page_number > 1;
+
+  // Check if there is a next page
+  const has_next_page = page_number < total_pages;
+
+  // Apply pagination
+  const users = await usersQuery
+    .skip((page_number - 1) * page_size)
+    .limit(page_size)
+    .lean();
+
+  const data = [];
+  for (let i = 0; i < users.length; i += 1) {
+    const user = users[i];
+    data.push({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  }
+  return {
+    page_number: parseInt(page_number),
+    page_size: parseInt(page_size),
+    count: users.length,
+    total_pages,
+    has_previous_page,
+    has_next_page,
+    data,
+  };
 }
 
 /**
--- src/api/components/users/users-service.js
+++ src/api/components/users/users-service.js
@@ -2,76 +2,35 @@ const usersRepository = require('./users-repository');
 const { hashPassword, passwordMatched } = require('../../../utils/password');
 
 /**
- * Get list of users
- * @param
+ * Get list of users with pagination and filter
+ * @param {number} page_number - Page number
+ * @param {number} page_size - Page size
  * @param {string} search - Search options
+ * @param {string} sort - Sort options
  * @returns {Array}
  */
 async function getUsers({ page_number, page_size, search, sort }) {
   let query = {};
+  let sortOptions = {};
 
-  let users = await usersRepository.getUsers();
-
-  // make the search function
+  // Make the search function
   if (search) {
     const [searchField, searchKey] = search.split(':');
     query[searchField] = { $regex: new RegExp(searchKey, 'i') };
   }
 
   // Apply sorting function
-  let sortOptions = {};
   if (sort) {
     const [sortField, sortOrder] = sort.split(':');
     sortOptions[sortField] = sortOrder === 'asc' ? 1 : -1;
   }
 
-  if (Object.keys(sortOptions).length !== 0) {
-    const sortField = Object.keys(sortOptions)[0]; // Get sortField directly
-    const sortOrder = sortOptions[sortField] === 1 ? 'asc' : 'desc';
-
-    users.sort((a, b) => {
-      const aValue = a[sortField];
-      const bValue = b[sortField];
-      return sortOrder === 'asc'
-        ? aValue.localeCompare(bValue)
-        : bValue.localeCompare(aValue);
-    });
-  }
-
-  //count all the users
-  const count = users.length;
-
-  //calculate total pages
-  const total_pages = Math.ceil(count / page_size);
-
-  //Check if there is a previous page
-  const has_previous_page = page_number > 1;
-
-  //Check if there is a next page
-  const has_next_page = page_number < total_pages;
-
-  //apply the pagination
-  users = users.slice((page_number - 1) * page_size, page_number * page_size);
-
-  const data = [];
-  for (let i = 0; i < users.length; i += 1) {
-    const user = users[i];
-    data.push({
-      id: user.id,
-      name: user.name,
-      email: user.email,
-    });
-  }
-
-  return {
-    page_number: parseInt(page_number),
-    page_size: parseInt(page_size),
-    count: data.length,
-    total_pages,
-    has_previous_page,
-    has_next_page,
-    data,
-  };
+  return await usersRepository.getUsers({
+    page_number,
+    page_size,
+    query,
+    sortOptions,
+  });
 }
 
 /**
